d44f2eab42064ba3b9c0fcb137144e0269b39e07
drumer2142 <fotis360@windowslive.com>
2017-05-25 22:23:15 +0300

    start API

---
+++ alarm_api/alarm_api.py
@@ -0,0 +1,41 @@
+from pymongo import MongoClient
+
+class alarm_api:
+
+    db = ""
+
+    def __init__(self, host, port):
+
+        self.host = host
+        self.port = port
+
+
+    def MongoConnect(self):
+        client = MongoClient(self.host, self.port)
+        db = client.Arduino_Alarm
+
+    def setCollection(self, collection_name):
+        self.collection_name = collection_name
+
+    def getCollection(self):
+        return self.collection_name
+
+    def makeTestQuery():
+        collection = self.collection_name
+
+    def test_print(self):
+        print(self.db)
+
+
+
+def main():
+    api = alarm_api('localhost', 27017)
+    api.MongoConnect()
+    api.setCollection('alarm_data')
+    api.test_print()
+    #name = api.getCollection()
+    #print(name)
+
+
+if __name__ == '__main__':
+    main()
