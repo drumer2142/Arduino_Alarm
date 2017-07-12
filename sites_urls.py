91d2846dc5e57f21761a41fe4a1707d5f55a5806
drumer2142 <fotis360@windowslive.com>
2017-07-12 05:56:13 +0300

    Indentation Error Fix

--- sites_urls.py
+++ sites_urls.py
@@ -1,10 +1,14 @@
 import urllib.request
 
-
 def scrap_page(url):
-    web_data = urllib.request.urlopen(url)
-    str_data = web_data.read()
-    return str_data
+	
+	try:
+		web_data = urllib.request.urlopen(url)
+		str_data = web_data.read()
+		return str_data
+  
+	except:
+		print("Something went wrong trying tp request the url open")
 
 
 def get_site_code(url):
