51104c57e99803a290112e4992967f370714874c
drumer2142 <fotis360@windowslive.com>
2018-04-04 04:18:11 +0300

    Mongo Backup script

---
+++ backup_scripts/mongoExport.py
@@ -0,0 +1,13 @@
+# the script is intented for exporting the specified collection
+# for backup and security perposes. It is triggered with a cronjob and can be modified
+# to suite the server's setup
+
+import os
+
+Database = "Arduino_Alarm"
+Collection = "alarm_data"
+export_path = "logs/Mongo_Backups/mongo_export_backup.json"
+
+export_command = "mongoexport --db "+Database+" --collection "+Collection+" --out "+export_path
+
+os.system(export_command)
