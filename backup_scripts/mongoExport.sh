3a3635cf415fa293466fcc4a658146fac3ff0c47
drumer2142 <fotis360@windowslive.com>
2018-05-13 13:39:20 +0300

    bash script modification

--- backup_scripts/mongoExport.sh
+++ backup_scripts/mongoExport.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-python3 mongoExport.py
+
+project_dir=/srv/python_projects/Arduino_Alarm/backup_scripts
+
+python3 $project_dir/mongoExport.py
+
+sleep 15
+
+cp $project_dir/logs/Mongo_Backups/mongo_export_backup.json /var/www/html
+
+echo Job Done.
